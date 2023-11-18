3259da5e39541b0c8a29123dee18bf34c8a2c5b9
atar1boy <atar1boy@yandex.ru>
2023-11-18 17:28:00 +0300

    Добавлен роутер user

--- app/api/endpoints/__init__.py
+++ app/api/endpoints/__init__.py
@@ -1,1 +1,2 @@
 from .donation import router as donation_router  # noqa
+from .user import router as user_router  # noqa
--- app/api/routers.py
+++ app/api/routers.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import (donation_router)
+from app.api.endpoints import (donation_router, user_router)
 
 main_router = APIRouter()
 main_router.include_router(
     donation_router, prefix='/donation', tags=['Donations']
 )
+main_router.include_router(user_router)
